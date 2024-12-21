9acdfeb0cae38461fe41df4b43e3b21cd042acc7
Your Name <emirmarash@gmail.com>
2024-12-21 17:02:55 +0600

    added header-component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div>
-
+        <header>
+            <a style={{marginright: '10px'}} href="/https://www.youtube.com/">YouTube</a>
+            <a style={{marginright: '10px'}} href='https://www.google.com/'>Google</a>
+            <a style={{marginright: '10px'}} href='https://www.facebook.com/'>Facebook</a>
+            <a style={{marginright: '10px'}} href='https://www.instagram.com/'>Instagram</a>
+        </header>
     </div>
   );
 }
