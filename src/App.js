ffc63de04cb4482a35c96de4cb9969ceee8bd110
Your Name <emirmarash@gmail.com>
2024-12-21 17:17:45 +0600

    added content

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div>
-
+        <content>
+          <div>
+            <h1>My App</h1>
+            <p>This is my app.</p>
+            <p>It is a simple app.</p>
+            <p>It is a simple app.</p>
+            <p>It is a simple app.</p>
+          </div>
+        </content>
     </div>
   );
 }
