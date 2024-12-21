bceca13a3def4f5ac188daa3da971647fc098f62
Your Name <emirmarash@gmail.com>
2024-12-21 17:12:30 +0600

    added button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { useState } from 'react';
 
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
     <div>
-
+        <button>
+            <h1>Счетчик: {count}</h1>
+            <button onClick={increment}>Увеличить</button>
+            <button onClick={decrement}>Уменьшить</button>
+        </button>
     </div>
   );
 }
